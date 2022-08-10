92cb1039b83ca93fdaace49bd36d96fcc4314ca7
httpmeggo <78925279+httpmeggo@users.noreply.github.com>
2022-08-10 18:59:28 -0400

    initial copy over

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+import java.util.Date;
+import java.util.List;
+
+public abstract class Book {
+  private String ISBN;
+  private String title;
+  private String subject;
+  private String publisher;
+  private String language;
+  private int numberOfPages;
+  private List<Author> authors;
+}
+
+class BookItem extends Book { //removed public
+  private String barcode;
+  private boolean isReferenceOnly;
+  private Date borrowed;
+  private Date dueDate;
+  private double price;
+  private BookFormat format;
+  private BookStatus status;
+  private Date dateOfPurchase;
+  private Date publicationDate;
+  private Rack placedAt;
+
+  public boolean checkout(String memberId) {
+    if(bookItem.getIsReferenceOnly()) {
+      ShowError("This book is Reference only and can't be issued");
+      return false;
+    }
+    if(!BookLending.lendBook(this.getBarCode(), memberId)){
+      return false;
+    }
+    this.updateBookItemStatus(BookStatus.LOANED);
+    return true;
+  }
+}
+
+class Rack { //removed public
+  private int number;
+  private String locationIdentifier;
+}
