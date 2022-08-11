487b9bd475db92adefd1b974e3caa8ad0b167397
httpmeggo <78925279+httpmeggo@users.noreply.github.com>
2022-08-11 12:50:54 -0400

    Converted Catalogue class to Library class
    basically they will be combined. we just need to add a library function that gives info about the library (like its address and name and stuff)

--- src/Library.java
+++ src/Library.java
@@ -1,11 +1,28 @@
-import java.util.Date;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-
-public class Library {
-	private String name;
-	private Address address;
+public class Library implements Serializable {
+	private List<Book> collection;
 	
-	public Address getAddress() {
-		return address;
+	public Library() {
+		List<Book> collection = new ArrayList<Book>();
+	}
+	
+	public void addBook(Book book) {
+		collection.add(book);
+	}
+	
+	@Override
+	public String toString() {
+		String total = "\n";
+		//iterate using Iterator
+		Iterator<Book> i = collection.iterator();
+		while(i.hasNext()) {
+			Book b = (Book) i.next();
+			total = total + b.toString();
+		}
+		return total;
 	}
 }
