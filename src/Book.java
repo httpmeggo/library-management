e76d1ef051630362f8c95208af276871100190e0
httpmeggo <78925279+httpmeggo@users.noreply.github.com>
2022-08-11 11:41:08 -0400

    Deleted BookItem class and simplified Book class
    Simplified library process by deleting BookItem class. Implemented Serializable to Book class to keep track of books in library even when program is exited. Updated checkout method.

--- src/Book.java
+++ src/Book.java
@@ -1,48 +1,71 @@
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
 
-public abstract class Book {
-  private String ISBN;
-  private String title;
-  private String subject;
-  private String publisher;
-  private String language;
-  private int numberOfPages;
-  private List<Author> authors;
+public abstract class Book implements Serializable {
+	private String ISBN;
+	private String title;
+	private String author;
+	private String subject;
+	private String publisher;
+	private String language;
+	private int numberOfPages;
+	private double price;
+	private BookStatus status;
+
+	public Book() {
+		ISBN = "0";
+		title = null;
+		author = null;
+		subject = null;
+		publisher = null;
+		language = null;
+		numberOfPages = 0;
+		price = 0;
+		status = BookStatus.AVAILABLE;
+	}
+
+	public Book(String newISBN, String newTitle, 
+			String newAuthor, String newSubject, 
+			String newPublisher, String newLanguage,
+			int newNumOfPages, double newPrice, BookStatus newStatus) {
+		this.ISBN = newISBN;
+		this.title = newTitle;
+		this.author = newAuthor;
+		this.subject = newSubject;
+		this.publisher = newPublisher;
+		this.language = newLanguage;
+		this.numberOfPages = newNumOfPages;
+		this.price = newPrice;
+		this.status = newStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "\nTitle: " + title + "\nAuthor: " + author + "\nISBN: " 
+				+ ISBN + "\nSubject: " + subject + "\nPublisher: " + 
+				publisher + "\nLanguage: " + language + 
+				"\nNumber of Pages: " + numberOfPages + "\nPrice: " + price 
+				+ "\nStatus: " + status + "\n";
+	}
+
+	//GETTERS AND SETTERS
+
+	public void updateBookItemStatus(BookStatus stat) {
+		this.status = stat;
+	}
+
+
+	//METHODS
+	public boolean checkout(String isbn) {
+		if(isbn.compareTo(this.ISBN) == 0) {
+			System.out.println("You have successfully checked out " + this.title
+					+ "\nPlease return the book in two weeks.");
+			this.updateBookItemStatus(BookStatus.LOANED);
+			return true;
+		}
+		System.out.println("Cannot complete checkout. Please try again.");
+		return false;
+	}
 }
 
-class BookItem extends Book { //removed public
-  private String barcode;
-  private boolean isReferenceOnly;
-  private Date borrowed;
-  private Date dueDate;
-  private double price;
-  private BookFormat format;
-  private BookStatus status;
-  private Date dateOfPurchase;
-  private Date publicationDate;
-  private Rack placedAt;
-  private BookItem reservationArray[];
-
-  //GETTERS AND SETTERS
-  public String getBarCode(){
-	return barcode;
-	  
-  }
-  public void updateBookItemStatus(BookStatus stat) {
-	  this.status = stat;
-  }
-
-
-  public boolean checkout(String memberId) {
-    if(bookItem.getIsReferenceOnly()) {
-      ShowError("This book is Reference only and can't be issued");
-      return false;
-    }
-    if(!BookLending.lendBook(this.getBarCode(), memberId)){
-      return false;
-    }
-    this.updateBookItemStatus(BookStatus.LOANED);
-    return true;
-  }
-}
